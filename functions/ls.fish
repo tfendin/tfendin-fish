b16e3e7ac742753808b910adf16ca80ccb793310
Tobias Fendin <tobias.fendin@mail.com>
2025-08-24 19:24:36 +0200

    Don't pass --ignore-backups to ls if -a or -A is given

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,3 +1,21 @@
 function ls --description 'My incarnation of ls'
-    command ls --color=auto --group-directories-first -BF $argv
+    argparse --ignore-unknown 'a/all' 'A/almost-all' -- $argv
+
+    set -l args --color=auto --group-directories-first --classify
+
+    if set -ql _flag_a; or set -ql _flag_A
+        # noop
+    else
+        set -a args --ignore-backups
+    end
+
+    # argparse removes -a or -A from $argv, add them again
+    if set -ql _flag_a
+        set -a args -a
+    end
+    if set -ql _flag_A
+        set -a args -A
+    end
+
+    command ls $args $argv
 end
